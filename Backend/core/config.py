77186fd669c9f1d940e9163f1c3b5f434da021a5
zeroxlearner <zeroxlearner@gmail.com>
2021-07-09 16:42:09 +0500

    Add remaining files for db connection

--- Backend/core/config.py
+++ Backend/core/config.py
@@ -1,7 +1,19 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
+
+    POSTGRES_USER: str = os.getenv("POSTGRES_USERNAME")
+    POSTGRES_PASS: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "job_board_db")
+    DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 settings = Settings()
--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
+
 
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+
+@app.on_event("startup")
+async def start_app():
+    # create db tables on initializing
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
 
 
 @app.get("/")
 def hello_api():
     return {"message": "Welcome to Job Board"}
+
+
+
