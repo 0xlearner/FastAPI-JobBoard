f87138c3ca078bb1c2865050cdbc744cfae781de
zeroxlearner <zeroxlearner@gmail.com>
2021-07-09 16:40:36 +0500

    Database Connection

---
+++ Backend/db/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+
+SQLALCHMEY_DATABASE_URL = settings.DATABASE_URL
+engine = create_async_engine(SQLALCHMEY_DATABASE_URL, future=True, echo=True)
+async_session = sessionmaker(engine, autocommit=False, autoflush=False, class_=AsyncSession)
